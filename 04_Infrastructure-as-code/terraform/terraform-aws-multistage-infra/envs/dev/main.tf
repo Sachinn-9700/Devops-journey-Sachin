6730df4b838c736517770a0f12caee7765b1d3d8
Sachinn-9700 <sachinn.bashetti@gmail.com>
2026-01-12 01:15:11 +0530

    Iac update

--- 04_Infrastructure-as-code/terraform/terraform-aws-multistage-infra/envs/dev/main.tf
+++ 04_Infrastructure-as-code/terraform/terraform-aws-multistage-infra/envs/dev/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "../../modules/vpc"
 
-  env       = var.env
+  env      = var.env
   app_name = var.app_name
   vpc_cidr = "10.0.0.0/16"
 }
@@ -10,7 +10,7 @@ module "vpc" {
 module "security_group" {
   source = "../../modules/security-group"
 
-  env       = var.env
+  env      = var.env
   app_name = var.app_name
   vpc_id   = module.vpc.vpc_id
 }
@@ -18,10 +18,10 @@ module "security_group" {
 module "ec2" {
   source = "../../modules/ec2"
 
-  env                 = var.env
+  env                = var.env
   app_name           = var.app_name
   instance_type      = var.instance_type
   key_name           = var.key_name
-  subnet_id           = module.vpc.public_subnet_id
+  subnet_id          = module.vpc.public_subnet_id
   security_group_ids = [module.security_group.sg_id]
 }
